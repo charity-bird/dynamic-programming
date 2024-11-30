3efb12b1b7c2d97b5749618ec40b466f1633af48
Danish Khan <khandanish5@outlook.com>
2024-11-30 16:18:46 -0500

    Added solution to Weighted Interval Scheduling problem. Added Readme.

---
+++ README.md
@@ -0,0 +1,20 @@
+# Dynamic Programming 
+
+A collection of interesting dynamic programming problems and their solutions in java.
+
+### Problem 1: Weighted Interval Scheduling
+
+Job j starts at sj, finishes at fj, and has weight or value vj. Two jobs compatible
+if they don't overlap. Goal: find maximum weight subset of mutually compatible jobs.
+
+> "Logic, after all, is a trick devised by the human mind to solve certain types of
+> problems. But mathematics is more than logic, it is logic plus the creative process.
+> How the logical devices that consititute the tools of mathematics are to be combined
+> to yield the desired results is not necessarily logical, no more than the writing of
+> a symphony is logical exersice, or the painting of a picture an exercise in
+> syllogisms." - Richard E. Bellman, 1972.
+
+> "Mathematics makes natural questions precise." - Richard E. Bellman
+
+> "Thoeries stand or fall, ultimately upon numbers." - Richard E. Bellman
+
---
+++ src/WeightedIntervalScheduling/WeightedIntervalScheduling.java
@@ -0,0 +1,122 @@
+package WeightedIntervalScheduling;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.TreeMap;
+
+class Job {
+    int startTime;
+    int endTime;
+    int value;
+    int label;
+    public Job(int startTime, int finishTime, int value) {
+        this.startTime = startTime;
+        this.endTime = finishTime;
+        this.value = value;
+    }
+}
+public class WeightedIntervalScheduling {
+
+    static TreeMap<Integer, Job> jobs;
+    static Integer[] M;
+
+    public void iterativeComputeOPT(int j) {
+        // Under review, unsure how to construct iterative approach
+        M[0] = 0;
+        for (int i = 0; i < jobs.size(); i++)
+            M[j] = Math.max(jobs.get(j).value + M[p(j)], M[j-1]);
+    }
+
+    public static void initializeM() {
+        int n = WeightedIntervalScheduling.jobs.size();
+        M = new Integer[n + 1];
+        for (int j = 1; j < n; j++)
+            M[j] = null;
+        M[0] = 0;
+    }
+
+    public static void findSolution(int j) {
+        if (j == 0) {
+
+        } else if (jobs.get(j).value + M[p(j)] > M[j-1]) {
+            System.out.print(j + " ");
+            findSolution(p(j));
+        } else {
+            findSolution(j - 1);
+        }
+    }
+    public static int MComputeOPT(int j) {
+        if (M[j] == null)
+            M[j] = Math.max(jobs.get(j).value + MComputeOPT(p(j)), MComputeOPT(j-1));
+        return M[j];
+    }
+    public static int computeOPT(int j) {
+        if (j == 0)
+            return 0;
+        else
+            return Math.max(jobs.get(j).value + computeOPT(p(j)), computeOPT(j-1));
+    }
+
+    public static int p(int label) {
+        int thisJobsStartTime = jobs.get(label).startTime;
+        for (Map.Entry<Integer, Job> entry : jobs.descendingMap().entrySet()) {
+            if (entry.getValue().endTime <= thisJobsStartTime) {
+                return entry.getKey();
+            }
+        }
+        return 0;
+    }
+
+    public static void labelJobsByFinishTimes(Job[] jobs) {
+        Arrays.sort(jobs, new Comparator<Job>() {
+            public int compare(Job a, Job b) {
+                return a.endTime - b.endTime;
+            }
+        });
+        for (int i = 0; i < jobs.length; i++) {
+            jobs[i].label = i;
+        }
+    }
+
+    public static void main(String[] args) {
+        Job j1 = new Job(0,6,4);
+        Job j2 = new Job(1,4,5);
+        Job j3 = new Job(3,5,4);
+        Job j4 = new Job(3,8,3);
+        Job j5 = new Job(4,7,1);
+        Job j6 = new Job(5,9,2);
+        Job j7 = new Job(6,10,3);
+        Job j8 = new Job(8,11,5);
+        Job[] jobsArray = {j1, j2, j3, j4, j5, j6, j7, j8};
+        labelJobsByFinishTimes(jobsArray);
+        for (int i = 0; i < jobsArray.length; i++) {
+            System.out.printf("%d start:%d, end:%d, value:%d\n", jobsArray[i].label, jobsArray[i].startTime, jobsArray[i].endTime, jobsArray[i].value);
+        }
+        WeightedIntervalScheduling.jobs = new TreeMap<Integer, Job>();
+        for (int i = 0; i < jobsArray.length; i++) {
+            WeightedIntervalScheduling.jobs.put(jobsArray[i].label+1, jobsArray[i]);
+        }
+        System.out.println("TreeMap:");
+        for (Map.Entry<Integer, Job> entry : WeightedIntervalScheduling.jobs.descendingMap().entrySet()) {
+            int label = entry.getValue().label;
+            int startTime = entry.getValue().startTime;
+            int endTime = entry.getValue().endTime;
+            int value = entry.getValue().value;
+            System.out.printf("%d, start:%d, end:%d, value:%d\n", label, startTime, endTime, value);
+        }
+        System.out.println(p(8));
+        System.out.println(p(2));
+        System.out.println(computeOPT(8));
+
+        initializeM();
+
+        System.out.println(MComputeOPT(8));
+
+        System.out.println("Finding solution for j=8");
+        findSolution(8);
+
+
+    }
+
+}
